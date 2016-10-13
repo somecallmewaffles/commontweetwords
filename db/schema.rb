07123780fce1dcf234552b578f6def9e957c75ab
David Reaves <makemesomewaffles@gmail.com>
2016-10-12 23:47:12 -0400

    Change to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20161013031302) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cities", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
