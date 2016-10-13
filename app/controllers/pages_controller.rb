2bf0c00730fb418d6273d0c09547c7fe94a85bdb
David Reaves <makemesomewaffles@gmail.com>
2016-10-12 23:07:59 -0400

    Breakdown tweets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
 
   def index
-    client = Twitter::REST::Client.new do |config|
+    @client = Twitter::REST::Client.new do |config|
       config.consumer_key    = "e1RG32tKYnJodvlMXbuxI1Mej"
       config.consumer_secret = "5G023ZsfQ59z4CvcwggX41lJdo4t1qRpioXtgKUFHcFz2GT2jY"
+      config.bearer_token = "AAAAAAAAAAAAAAAAAAAAAPSkxQAAAAAAeGwD0ir%2FvmBwRWwlZAP0BH09PzU%3D2yR0ABQzWefgytuYtUcc6YxZp8MB8RWOpXbZUuVYsscuc9T7Wr"
     end
+    @client.user("hellyeawaffles")
   end
 
 end
